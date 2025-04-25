1d0812882f8a56af25c578bab84fe048dedc86dd
Sagarbharti01 <166221971+Sagarbharti01@users.noreply.github.com>
2025-04-25 23:52:55 +0530

    Merge Two Sorted Arrays Without Extra Space
    Given two sorted arrays a[] and b[] of size n and m respectively, the task is to merge both the arrays and rearrange the elements such that the smallest n elements are in a[] and the remaining m elements are in b[]. All elements in a[] and b[] should be in sorted order.
    
    Examples: 
    
    Input: a[] = [2, 4, 7, 10], b[] = [2, 3]
    Output: a[] = [2, 2, 3, 4], b[] = [7, 10] 
    Explanation: Combined sorted array = [2, 2, 3, 4, 7, 10], array a[] contains smallest 4 elements: 2, 2, 3 and 4, and array b[] contains remaining 2 elements: 7, 10.
    
    Input: a[] = [1, 5, 9, 10, 15, 20], b[] = [2, 3, 8, 13]
    Output: a[] = [1, 2, 3, 5, 8, 9], b[] = [10, 13, 15, 20]
    Explanation: Combined sorted array = [1, 2, 3, 5, 8, 9, 10, 13, 15, 20], array a[] contains smallest 6 elements: 1, 2, 3, 5, 8 and 9, and array b[] contains remaining 4 elements: 10, 13, 15, 20.
    
    Input: a[] = [0, 1], b[] = [2, 3]
    Output: a[] = [0, 1], b[] = [2, 3] 
    Explanation: Combined sorted array = [0, 1, 2, 3], array a[] contains smallest 2 elements: 0 and 1, and array b[] contains remaining 2 elements: 2 and 3.
    
    Table of Content
    
    Using Insert of Insertion Sort
    Using n-th smallest element
    By Swap and Sort
    Using Gap method
    --------------------------------------------------
    Using Gap method
    The idea is to assume the two arrays as a single continuous array of size n + m andsort it using gap method of shell sort. Here we need to adjust the indices according to whether it lies in a[] or b[]. And if indices lies in b[] then adjust the indices by subtracting n from it.
    
    Assume the two arrays as a single continuous array of size n + m and find the gap value, gap = ceil((n + m)/2 )
    Until the gap doesn't become zero, perform the following operations:
    Take two pointers left and right and place them at index 0 and (left + gap) index respectively.
    Run a while loop until right is less than len (n + m).
    Their are 3 different cases inside this while loop.
    When both the left and right pointers are in the a[]. Then compare the a[left] and a[right]. If a[left] > a[right], then swap the a[left] and a[right].
    When the left pointer is in a[] and right pointer is in b[] and if a[left] > b[right-m], then swap the a[left] and b[right-m].
    When both the left and right pointers are in the b[] and if a[left] > b[right-m], then swap the a[left] and b[right-m].
    If the right pointer reaches the end i.e. m + n, decrement the gap value by ceil(gap/2).
    
    #

---
+++ mergeNoExtraSpace.java
@@ -0,0 +1,44 @@
+public class mergeNoExtraSpace {
+
+    public static void mergeNoExtraSpace(int[] a, int[] b) {
+
+        int n = a.length;
+        int m = b.length;
+        int gap = (n+m+1)/2;
+while(gap > 0){
+            int i = 0, j= gap;
+            while(j < n+m){
+                if(j<n && a[i] > a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+                else if(i<n && j>=n && a[i] > b[j-n]){
+                    int temp = a[i];
+                    a[i] = b[j-n];
+                    b[j-n] = temp;
+                }
+                else if(i >=n && b[i-n] > b[j-n]){
+                    int temp = b[i-n];
+                    b[i-n] = b[j-n];
+                    b[j-n] = temp;
+                }
+                i++;
+                j++;
+            }
+            if(gap == 1)
+                break;
+            gap = (gap+1)/2;
+}
+}
+public static void main(String [] args){
+    int[] a = {1, 5, 9, 10, 15, 20};
+    int[] b = {2, 3, 8, 13};
+    mergeNoExtraSpace(a, b);
+    for (int ele : a)
+            System.out.print(ele + " ");
+        System.out.println();
+        for (int ele : b)
+            System.out.print(ele + " ");
+}
+}
